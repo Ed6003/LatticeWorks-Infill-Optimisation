d6fc991cce87e5ba7212782c99a989ed4c9d30b5
Ed <ruscoedo@gmail.com>
2025-02-13 17:13:08 +0000

    Moved saveFigures to function

---
+++ saveFigures.m
@@ -0,0 +1,30 @@
+function nFigures = saveFigures(savePath,doSaveFigures,nFigures)
+    figsavePath = fullfile(savePath, 'Figures');
+    figHandles = findobj('Type', 'figure');
+    
+    if saveFigures
+        % Iterate over each figure
+        for j = 1:length(figHandles)
+            % Get the current figure handle
+            figHandle = figHandles(j);
+            
+            % Generate a unique filename based on the figure number
+            figName = sprintf('Figure_%d', figHandle.Number + nFigures);
+            
+            % Save the figure as a .fig file
+            savefig(figHandle, fullfile(figsavePath, [figName, '.fig']));
+            
+            % Save the figure as a .png file
+            saveas(figHandle, fullfile(figsavePath, [figName, '.png']));
+            
+            % Close the figure
+            close(figHandle);
+        end
+    else
+        for j = 1:length(figHandles)
+            close(figHandle);
+        end
+    end
+end
+
+nFigures = length(figHandles);
