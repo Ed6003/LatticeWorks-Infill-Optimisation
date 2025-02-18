74f2a5365cea2997de5100b60013a9847302e1d3
Ed <ruscoedo@gmail.com>
2025-02-18 17:39:53 +0000

    Log Update - added timestamps

--- processLogFiles.m
+++ processLogFiles.m
@@ -1,9 +1,7 @@
 cd('C:\Users\rusco\OneDrive - University of Warwick\Admin\Archives\Documents\GitHub\LatticeWorks');
 checkSubfolders = true;
 
-saveFolder = 'C:\Users\rusco\OneDrive - University of Warwick\Admin\Archives\Documents\GitHub\LatticeWorks';
-saveFolder = fullfile(saveFolder, 'Logs');
-
+saveFolder = 'C:\Users\rusco\OneDrive - University of Warwick\Admin\Archives\Documents\GitHub\LatticeWorks\Logs';
 masterFile = fullfile(saveFolder, 'master.csv');
 
 logsDir = 'C:\Users\rusco\OneDrive - University of Warwick\Admin\Archives\Documents\GitHub\LatticeWorks\Logs';
@@ -27,38 +25,38 @@ end
 [~, sortIdx] = sort([subfolders.datenum]); % Sort by creation time
 subfolders = subfolders(sortIdx);
 
-if checkSubfolders
-    for i = 1:length(subfolders)
-        recentSubfolder = subfolders(i).name;
-        recentSubfolderPath = fullfile(logsDir, recentSubfolder);
-        
-        % Locate the .blg file in the most recent subfolder
-        blgFiles = dir(fullfile(recentSubfolderPath, '*.blg'));
-        
-        % Check if a .blg file exists
-        if isempty(blgFiles)
-            error('No .blg file found in the most recent subfolder.');
-        end
-        
-        % Assume there's only one .blg file; if multiple, process the first one
-        blgFile = fullfile(recentSubfolderPath, blgFiles(1).name);
-        
-        % Define the output .csv file path
-        csvName = sprintf('%.0f', posixtime(datetime('now', 'TimeZone', 'UTC')));
-        csvFile = fullfile(logsDir, [csvName, '.csv']);
-        
-        % Convert .blg to .csv using the relog command
-        relogCmd = sprintf('relog "%s" -f csv -o "%s"', blgFile, csvFile);
-        system(relogCmd);
-        
-        % Delete the processed subfolder
-        try
-            rmdir(recentSubfolderPath, 's');
-        catch
-            warning('Failed to delete the folder: %s\nError: %s', recentSubfolderPath, ME.message);
-        end
-    end
-end
+% if checkSubfolders
+%     for i = 1:length(subfolders)
+%         recentSubfolder = subfolders(i).name;
+%         recentSubfolderPath = fullfile(logsDir, recentSubfolder);
+% 
+%         % Locate the .blg file in the most recent subfolder
+%         blgFiles = dir(fullfile(recentSubfolderPath, '*.blg'));
+% 
+%         % Check if a .blg file exists
+%         if isempty(blgFiles)
+%             error('No .blg file found in the most recent subfolder, check folder permissions. (Admin only by default)');
+%         end
+% 
+%         % Assume there's only one .blg file; if multiple, process the first one
+%         blgFile = fullfile(recentSubfolderPath, blgFiles(1).name);
+% 
+%         % Define the output .csv file path
+%         csvName = sprintf('%.0f', posixtime(datetime('now', 'TimeZone', 'UTC')));
+%         csvFile = fullfile(logsDir, [csvName, '.csv']);
+% 
+%         % Convert .blg to .csv using the relog command
+%         relogCmd = sprintf('relog "%s" -f csv -o "%s"', blgFile, csvFile);
+%         system(relogCmd);
+% 
+%         % Delete the processed subfolder
+%         try
+%             % rmdir(recentSubfolderPath, 's');
+%         catch
+%             warning('Failed to delete the folder: %s\nError: %s', recentSubfolderPath, ME.message);
+%         end
+%     end
+% end
 
 % Get list of CSV files sorted by creation date
 fileList = dir(fullfile(saveFolder, '*.csv'));
@@ -73,15 +71,19 @@ for i = 1:length(fileList)
     % Read the CSV file
     data = readtable(filePath);
 
-    colIdx = contains(data.Properties.VariableNames, 'Total') & contains(data.Properties.VariableNames, 'ProcessorTime');
-    
-    columnName = data.Properties.VariableNames{colIdx};
+    colIdx = contains(data.Properties.VariableNames, 'Total') & contains(data.Properties.VariableNames, 'ProcessorTime') | contains(data.Properties.VariableNames, 'GMTStandardTime');
+
+    columnName = data.Properties.VariableNames(colIdx);
     % columnName = "x__EDVICTUS_Processor__Total___ProcessorTime";
     data = data(:, columnName);
 
-    data = fillmissing(data, 'constant', 0); % change NaN to 0
+    data.Properties.VariableNames(1) = "Time";
+    data.Properties.VariableNames(2) = "Processor Time %";
 
-    data.Properties.VariableNames(1) = "Processor Time %";
+    % data = fillmissing(data, 'constant', 0); % change NaN to 0
+
+    numericVars = varfun(@isnumeric, data, 'OutputFormat', 'uniform');
+    data(:, numericVars) = fillmissing(data(:, numericVars), 'constant', 0);
     
     % Append to master.csv (create if it doesn't exist)
     if exist(masterFile, 'file')
@@ -91,7 +93,7 @@ for i = 1:length(fileList)
     end
     
     % Delete the processed file
-    delete(filePath);
+    % delete(filePath);
 end
 
 disp('Processing complete. All files merged and deleted.');
